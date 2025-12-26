1cdeb0113db50d6f6d3ed2a4726f842fb34b3d11
vairan9 <ohioresident69420@gmail.com>
2025-12-26 11:35:22 +0100

    Generate models in the html
    Website v1.0.1

--- infiniti_vacation/urls.py
+++ infiniti_vacation/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import website_home
+from .views import home
 
 app_name = "infiniti_vacation"
-
 urlpatterns = [
-    path("", website_home, name="home"),
+    path("", home, name="home"),
 ]
--- infiniti_vacation/views.py
+++ infiniti_vacation/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from .models import Apartment
 
-def website_home(request):
-    return render(request, "infiniti_vacation/page.html")
+def home(request):
+    apartments = Apartment.objects.filter(is_active=True).order_by("name")
+    return render(request, "infiniti_vacation/page.html", {"apartments": apartments})
