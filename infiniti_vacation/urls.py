757016e7badc493ff7a8de6329285af59336246f
vairan9 <ohioresident69420@gmail.com>
2025-12-28 20:54:27 +0100

    Hotfix

--- infiniti_vacation/urls.py
+++ infiniti_vacation/urls.py
@@ -5,7 +5,7 @@ app_name = "infiniti_vacation"
 
 urlpatterns = [
     path("", home, name="home"),
-    path("website/login/", website_login, name="website_login"),
-    path("website/register/", website_register, name="website_register"),
-    path("website/logout/", website_logout, name="website_logout"),
+    path("login/", website_login, name="website_login"),
+    path("register/", website_register, name="website_register"),
+    path("logout/", website_logout, name="website_logout"),
 ]
